a97a874a29d55327135bf6839c69cf6da14560ee
mnhng1 <minhnguyentuong1@gmail.com>
2024-09-07 13:53:12 -0400

    Migrate nodejs to django

---
+++ NQS/NQS/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for NQS project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.shortcuts import render
+from django.urls import path
+
+def index(request):
+    return render(request, 'build/index.html')
+
+urlpatterns = [
+    path('', index),
+    # Add other API paths here as needed
+]
--- backend/src/routes/search.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Placeholder route
-router.post('/', (req, res) => {
-  res.send('Search documents');
-});
-
-module.exports = router;
